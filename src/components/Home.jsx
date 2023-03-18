8f37835d33ddd53d93cd1fc9ec0411cec35adf4e
Cristian Rodriguez <cristianrodrig149@gmail.com>
2023-03-18 15:19:33 +0100

    ✨ feat(About): Add  component About

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,15 +5,12 @@ const Home = () => {
 	return (
 		<section name='home' className='flex h-screen w-full bg-slate-900 '>
 			<div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row '>
-				<div className='flex flex-col justify-center '>
+				<div className='flex flex-col justify-center mt-32 '>
 					<h2 className='text-white text-4xl sm:text-7xl font-bold '>
-						Desarrollador Junior{' '}
+						Desarrollador Web Junior{' '}
 					</h2>
 					<p className='text-slate-400 py-4 max-w-md'>
-						altamente analítico y resolutivo con una sólida formación académica
-						en tecnologías web. Con habilidades en análisis y resolución de
-						problemas, me enfoco en crear soluciones eficientes y de alta
-						calidad para satisfacer las necesidades de los usuarios.
+					Tengo habilidades en análisis y resolución de problemas, y estoy altamente familiarizado con las tecnologías HTML, CSS, JavaScript, SASS, React, Node, SQL, Git y Scrum.
 					</p>
 					<div>
 						<button className='
@@ -40,7 +37,7 @@ const Home = () => {
 					<img
 						src={pictureHome}
 						alt='profile'
-						className='rounded-2xl mx-auto mt-4 w-3/4 md:w-full'
+						className='rounded-2xl mx-auto mt-4 w-2/3 md:w-full'
 					/>
 				</div>
 			</div>
